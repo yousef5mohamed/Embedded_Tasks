c8924ea9dbe32799033a907796e187091a661e96
yousef5mohamed <yousefmohammed867@gmail.com>
2026-08-28 20:42:59 +0300

    Add Assignment 3

---
+++ Assignment_3/main.c
@@ -0,0 +1,9 @@
+#include "MCAL/ADC/ADC_Interface.h"
+#include "MCAL/DIO/DIO_Interface.h"
+#include "MCAL/EXTI/EXTI_Interface.h"
+#include "MCAL/GIE/GIE_Interface.h"
+
+int main(void)
+{
+    // Your application code
+}
